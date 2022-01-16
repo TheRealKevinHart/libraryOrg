0c5bd827c4477a8b0d4ac92b054a1cc6f62996e5
wsfer <wsfer@DESKTOP-R962GR5.Home>
2022-01-15 20:39:12 -0500

    Initial Spring Boot Program

---
+++ src/main/java/com/springboot/libraryOrg/LibraryOrgApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.libraryOrg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryOrgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryOrgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/libraryOrg/LibraryOrgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.libraryOrg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryOrgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
