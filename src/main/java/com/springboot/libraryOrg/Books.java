2bbfd15a7ae7200ec5479a482c570ffd2662ae34
wsfer <wsfer@HoseRunners-PC.Home>
2022-03-02 16:23:11 -0500

    ThymeLeaf and Dependency additions

--- src/main/java/com/springboot/libraryOrg/Books.java
+++ src/main/java/com/springboot/libraryOrg/Books.java
@@ -1,42 +1,44 @@
 package com.springboot.libraryOrg;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity // This tells Hibernate to make a table out of this class
+//@Table(name = "librarydb")
 public class Books {
-  @Id
-  @GeneratedValue(strategy=GenerationType.AUTO)
-  private Integer id;
+	@Id //Specifies the primary key of an entity
+	@Column(name = "id") //Specified the mapped column for a persistent property or field.
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer id;
+	private String title;
+  	private String author;
 
-  private String title;
+  	public Integer getId() {
+	  return id;
+  	}
 
-  private String author;
+  	public void setId(Integer id) {
+  		this.id = id;
+  	}
 
-  public Integer getId() {
-    return id;
-  }
+  	public String getTitle() {
+  		return title;
+  	}
 
-  public void setId(Integer id) {
-    this.id = id;
-  }
+  	public void setTitle(String title) {
+  	  	this.title = title;
+  	}
 
-  public String getTitle() {
-    return title;
-  }
+  	public String getAuthor() {
+  		return author;
+  	}
 
-  public void setTitle(String title) {
-    this.title = title;
-  }
-
-  public String getAuthor() {
-    return author;
-  }
-
-  public void setAuthor(String author) {
-    this.author = author;
-  }
+  	public void setAuthor(String author) {
+  		this.author = author;
+  	}
 }
 
