aa6690dea4c1122f870233de2a64247e7c0a2012
wsfer <wsfer@LAPTOP-BRSIQCRB>
2022-03-15 14:59:45 -0400

    3/15/22

--- src/main/java/com/springboot/libraryOrg/controllers/BooksController.java
+++ src/main/java/com/springboot/libraryOrg/controllers/BooksController.java
@@ -4,11 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,17 +21,19 @@ public class BooksController {
 	@Autowired
 	private BooksService booksService;
 	
+	public BooksController(BooksService booksService) {
+		this.booksService = booksService;
+	}
+	
 	@RequestMapping("/getAll")
 	public String getAll(Model model) {
-//		List<Books> books = booksService.getAll();
-//		model.addAttribute("books", books);
-//		
-//		String username = "Root";
-//		model.addAttribute("username", username);
-//		
-//		return "books";
+		List<Books> books = booksService.getAll();
+		model.addAttribute("books", books);
 		
-		return findPaginated(1, model);
+		String username = "Root";
+		model.addAttribute("username", username);
+		
+		return "books";
 	}
 	
 	@RequestMapping("/getOne")
@@ -59,20 +58,5 @@ public class BooksController {
 	public String delete(Integer Id) {
 		booksService.delete(Id);
 		return "redirect:/books/getAll";
-	}
-	
-	@GetMapping("/page/{pageNo}")
-	public String findPaginated(@PathVariable(value="pageNo") int pageNo, Model model) {
-		int pageSize = 5;
-		
-		Page<Books> page = booksService.findPaginated(pageNo, pageSize);
-		List<Books> listBooks = page.getContent();
-		
-		model.addAttribute("currentPage", pageNo);
-		model.addAttribute("totalPages", page.getTotalPages());
-		model.addAttribute("totalItems", page.getTotalElements());
-		model.addAttribute("listBooks", listBooks);
-		
-		return "books";
 	}
 }
--- src/main/java/com/springboot/libraryOrg/services/BooksService.java
+++ src/main/java/com/springboot/libraryOrg/services/BooksService.java
@@ -4,9 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import com.springboot.libraryOrg.models.Books;
@@ -36,10 +33,5 @@ public class BooksService {
 	
 	public void delete(Integer Id) {
 		booksRepository.deleteById(Id);
-	}
-	
-	public Page<Books> findPaginated(int pageNo, int pageSize) {
-		Pageable pageable = PageRequest.of(pageNo - 1, pageSize);
-		return booksRepository.findAll(pageable);
 	}
 }
