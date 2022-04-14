3dc3485197ea86a181b461cd48490b196c3119d7
Walker-Ferencz1121 <wsferencz@gmail.com>
2022-04-14 16:00:42 -0400

    4/14/22

--- bookScanner/bookScanner/pyBookScanner.py
+++ bookScanner/bookScanner/pyBookScanner.py
@@ -2,7 +2,7 @@ import cv2
 import pytesseract
 pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
-img4 = cv2.imread('files\\book2.jpg')
+img = cv2.imread('files\\book2.jpg')
 
-text4 = pytesseract.image_to_string(img4)
-print(text4)
+text = pytesseract.image_to_string(img)
+print(text)
