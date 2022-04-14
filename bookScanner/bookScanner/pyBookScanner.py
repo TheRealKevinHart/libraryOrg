a102e803660835cd62ed0050975ae4889d39d3b7
Walker-Ferencz1121 <wsferencz@gmail.com>
2022-04-14 15:06:33 -0400

    4/14/22

---
+++ bookScanner/bookScanner/pyBookScanner.py
@@ -0,0 +1,8 @@
+import cv2
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+
+img = cv2.imread('book2.jpg')
+
+text = pytesseract.image_to_string(img)
+print(text)
