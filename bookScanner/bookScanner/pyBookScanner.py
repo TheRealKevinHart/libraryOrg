5e3d6d6855506f952dac30ac5cc3c1eb1f3f2dad
Walker-Ferencz1121 <wsferencz@gmail.com>
2022-04-14 18:26:43 -0400

    4/14/22

--- bookScanner/bookScanner/pyBookScanner.py
+++ bookScanner/bookScanner/pyBookScanner.py
@@ -1,8 +1,23 @@
-import cv2
-import pytesseract
-pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+# import cv2
+# import pytesseract
+# pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+#
+# img = cv2.imread('files\\book2.jpg')
+#
+# text = pytesseract.image_to_string(img)
+# print(text)
 
-img = cv2.imread('files\\book2.jpg')
-
-text = pytesseract.image_to_string(img)
-print(text)
+# import cgi,os
+#
+# print('content-type:text/html\r\n\r\n')
+#
+# form=cgi.FieldStorage()
+# fle=form['filename']
+#
+# fn=os.path.basename('fle.filename')
+# open("D:/EclipseWorkspace/libraryOrg/libraryOrg/temp/"+fn,"wb").write(fle.file.read())
+#
+# print('<html>')
+# print('<body><center>')
+# print('<img src=temp/%s>'%fn)
+# print('</center></body></html>')
--- src/main/java/com/springboot/libraryOrg/LibraryOrgApplication.java
+++ src/main/java/com/springboot/libraryOrg/LibraryOrgApplication.java
@@ -24,4 +24,5 @@ public class LibraryOrgApplication {
 			storageService.init();
 		};
 	}
+
 }
