1bc68a181c09e762e2f3d883aa24e389545af095
Walker_Ferencz1121 <wsferencz@gmail.com>
2022-04-21 20:50:37 -0400

    4/21/22

--- src/main/java/com/springboot/libraryOrg/reader/bookScanner.java
+++ src/main/java/com/springboot/libraryOrg/reader/bookScanner.java
@@ -7,17 +7,28 @@ import net.sourceforge.tess4j.*;
 public class bookScanner {
 	
 	public String booksScanner() {
-		File imageFile = new File("C:\\Users\\wsfer\\images\\book2.jpg");
-
+		//File imageFile = new File("C:\\Users\\wsfer\\images\\book4.pdf");
+		
+		File folder = new File("C:\\Users\\wsfer\\images\\");
+		File[] listOfFiles = folder.listFiles();
+		
+		for (File file: listOfFiles) {
+			if (file.isFile()) {
+				//File newFile = new File(file.getName());
+				folder = new File("C:\\Users\\wsfer\\images\\" + file.getName());
+				System.out.println(folder);
+			}
+		}
 
 		//Change Datapath based off of computer
 		//PC: "D:\\EclipseWorkspace\\libraryOrg\\libraryOrg\\tessdata"
 		//LAPTOP: "C:\\Users\\wsfer\\git\\libraryOrg\\tessdata"
 		ITesseract instance = new Tesseract();
-		instance.setDatapath("C:\\Users\\wsfer\\git\\libraryOrg\\tessdata");
+		instance.setDatapath("D:\\EclipseWorkspace\\libraryOrg\\libraryOrg\\tessdata");
 		
 		try {
-			String result = instance.doOCR(imageFile);
+			//String result = instance.doOCR(imageFile);
+			String result = instance.doOCR(folder);
 			return result;
 		}
 		
---
+++ src/main/java/com/springboot/libraryOrg/reader/bookScannerTwo.java
@@ -0,0 +1,50 @@
+package com.springboot.libraryOrg.reader;
+
+import java.io.File;
+
+import net.sourceforge.tess4j.*;
+
+public class bookScannerTwo {
+	
+	public static void main(String[] args) {
+		//File imageFile = new File("C:\\Users\\wsfer\\images\\book.jpg");
+		
+		File folder = new File("C:\\Users\\wsfer\\images\\");
+		File[] listOfFiles = folder.listFiles();
+		long lastModifiedTime = Long.MIN_VALUE;
+	    File chosenFile = null;
+	    
+	    if (listOfFiles != null) {
+			for (File file: listOfFiles) {
+				
+				if (file.lastModified() > lastModifiedTime) {
+					chosenFile = file;
+	                lastModifiedTime = file.lastModified();
+	                System.out.println(chosenFile);
+				}
+//				if (file.isFile()) {
+//					folder = new File("C:\\Users\\wsfer\\images\\" + file.getName());
+//					long lastModified = folder.lastModified();
+//					System.out.println("files " + folder);
+//					System.out.println("newest " + lastModified);
+//				}
+			}
+	    }
+
+
+		//Change Datapath based off of computer
+		//PC: "D:\\EclipseWorkspace\\libraryOrg\\libraryOrg\\tessdata"
+		//LAPTOP: "C:\\Users\\wsfer\\git\\libraryOrg\\tessdata"
+		ITesseract instance = new Tesseract();
+		instance.setDatapath("D:\\EclipseWorkspace\\libraryOrg\\libraryOrg\\tessdata");
+		
+		try {
+			String result = instance.doOCR(folder);
+			System.out.println(result);
+		}
+		
+		catch (TesseractException e) {
+			System.err.println(e.getMessage());
+		}
+	}
+}
